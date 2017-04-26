1445c9e8ef3f9288e2cf7c6b9270da0cb987788b
nick <nli8888@hotmail.com>
2017-04-26 15:41:24 +0100

    updated Data_str...

--- Data_structure_tutorial.R
+++ Data_structure_tutorial.R
@@ -1,2 +1,9 @@
-library(data.table)
-# 
+#library(data.table)
+library(rethomics)
+
+dt <- data.table(t=1:30,
+                 x=rnorm(30),
+                 y=rnorm(30),
+                 is_feeding = rnorm(30) >0
+)
+dt
