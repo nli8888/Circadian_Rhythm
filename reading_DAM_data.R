f4ed836236daa35c120e4088adf920668e0a1e44
nick <nli8888@hotmail.com>
2017-04-27 18:22:02 +0100

    reading_DAM_data updated

---
+++ reading_DAM_data.R
@@ -0,0 +1,20 @@
+#infile <- read.table("/media/nick/Data/Users/N/Documents/MSc_Bioinfo/2016/Data_Analysis_Project/Circadian_Rhythm/per_rescue_v2/120115A5M/120115A5mCtM007C01.txt", sep="\n")
+header = scan("/media/nick/Data/Users/N/Documents/MSc_Bioinfo/2016/Data_Analysis_Project/Circadian_Rhythm/per_rescue_v2/120115A5M/120115A5mCtM007C01.txt", what="", nmax= 1, sep="\n")
+infile = scan("/media/nick/Data/Users/N/Documents/MSc_Bioinfo/2016/Data_Analysis_Project/Circadian_Rhythm/per_rescue_v2/120115A5M/120115A5mCtM007C01.txt", what=1, skip=1, sep="\n")
+#header
+sample_freq = infile[2]
+sample_freq
+#typeof(infile)
+activity = infile[4:length(infile)]
+activity
+t_list = vector()
+t = 0
+for (i in activity){
+  #print(i)
+  t_list = c(t_list, t)
+  t = t + sample_freq
+}
+#t_list = c(t_list, sample_freq)
+t_list
+dt = data.table(activity=activity, t=t_list)
+dt
