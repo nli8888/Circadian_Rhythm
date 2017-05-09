d5e4fa9e571ac961a984b6fad3a39b7fe38176ac
nick <nli8888@hotmail.com>
2017-05-09 18:02:28 +0100

    updated

--- actoplot.R
+++ actoplot.R
@@ -1,4 +1,5 @@
 source("/media/nick/Data/Users/N/Documents/MSc_Bioinfo/2016/Data_Analysis_Project/Circadian_Rhythm/DAM1_reader.R")
+library(grid)
 actoplot = function(y,
                     data,
                     num_of_dup = "double", #can be "double", "triple" or "quad"
@@ -13,11 +14,10 @@ actoplot = function(y,
   dt[, t_round := t_round]
   dt[, hour := hour]
   dt[, day := day]
-  dt = dt[,.(experiment_id = experiment_id, machine_name = machine_name, region_id = region_id, date = date, sum_activity = sum(activity), hour = hour, day = day), by = t_round]
-  dt = unique(dt)
   setkeyv(dt, c("experiment_id", "region_id", "date", "machine_name"))
   if (mean == FALSE){
-    #dt = dt[, .(mean_activity = mean(activity), hour=hour), by = t_round]
+    dt = dt[,.(experiment_id = experiment_id, machine_name = machine_name, region_id = region_id, date = date, sum_activity = sum(activity), hour = hour, day = day), by = t_round]
+    dt = unique(dt)
   if (num_of_dup == "double"){
     dt2 = copy(dt)
     dt2 = dt2[,day := day-1]
@@ -27,6 +27,11 @@ actoplot = function(y,
     binddt = binddt[day>-1]
     binddt = binddt[, day_str := sprintf("day\n%03d",day)]
     x_scale = 0:8 * 6
+    p = ggplot(binddt, aes(x=hour, y=sum_activity)) + 
+      geom_col() +
+      facet_grid(day_str ~ .) + scale_x_continuous(name="time (h)",breaks = x_scale)+
+      scale_y_continuous(name="activity") +
+      theme(panel.spacing = unit(0, "lines"))
   } else if (num_of_dup == "triple"){
     dt2 = copy(dt)
     dt2 = dt2[,day := day-1]
@@ -35,10 +40,16 @@ actoplot = function(y,
     dt3 = dt3[,day := day-1]
     dt3 = dt3[,hour := hour + 24]
     dt = dt[day<(max(day)-1)]
+    dt2 = dt2[day<max(day)]
     binddt = rbind(dt, dt2, dt3)
     binddt = binddt[day>-1]
     binddt = binddt[, day_str := sprintf("day\n%03d",day)]
     x_scale = 0:12 * 6
+    p = ggplot(binddt, aes(x=hour, y=sum_activity)) + 
+      geom_col() +
+      facet_grid(day_str ~ .) + scale_x_continuous(name="time (h)",breaks = x_scale)+
+      scale_y_continuous(name="activity") +
+      theme(panel.spacing = unit(0, "lines"))
   } else if (num_of_dup == "quad"){
     dt2 = copy(dt)
     dt2 = dt2[,day := day-1]
@@ -50,23 +61,30 @@ actoplot = function(y,
     dt4 = dt4[,day := day-1]
     dt4 = dt4[,hour := hour + 24]
     dt = dt[day<(max(day)-2)]
+    dt2 = dt2[day<max(day)-1]
+    dt3 = dt3[day<max(day)]
     binddt = rbind(dt, dt2, dt3, dt4)
     binddt = binddt[day>-1]
     binddt = binddt[, day_str := sprintf("day\n%03d",day)]
     x_scale = 0:16 * 6
+    p = ggplot(binddt, aes(x=hour, y=sum_activity)) + 
+      geom_col() +
+      facet_grid(day_str ~ .) + scale_x_continuous(name="time (h)",breaks = x_scale)+
+      scale_y_continuous(name="activity") +
+      theme(panel.spacing = unit(0, "lines"))
   }
-  y_var = sum_activity
   } else if (mean == TRUE){
-    
+    #dt = dt[, .(mean_activity = mean(activity), hour=hour), by = t_round]
   }
-  # p = ggplot(binddt,aes(hour,ymax=y_var, ymin=min(y_var))) +
+  # p = ggplot(binddt,aes(hour,ymax=sum_activity, ymin=min(um_activity))) +
   #   geom_ribbon() +
   #   facet_grid(day_str ~ .) + scale_x_continuous(name="time (h)",breaks = x_scale)+
   #   scale_y_continuous(name="y")
-  p = ggplot(binddt, aes(x=hour, y=y_var)) + 
-      geom_col() +
-      facet_grid(day_str ~ .) + scale_x_continuous(name="time (h)",breaks = x_scale)+
-      scale_y_continuous(name="activity")
+  # p = ggplot(binddt, aes(x=hour, y=sum_activity)) + 
+  #     geom_col() +
+  #     facet_grid(day_str ~ .) + scale_x_continuous(name="time (h)",breaks = x_scale)+
+  #     scale_y_continuous(name="activity") +
+  #     theme(panel.spacing = unit(0, "lines"))
   p
   #return(binddt)
 }
