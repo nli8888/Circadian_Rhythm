12c47294da12c3d074110b96de05292dd96cbae4
nick <nli8888@hotmail.com>
2017-06-14 19:16:51 +0100

    updated

--- My_webapp/app.R
+++ My_webapp/app.R
@@ -5,11 +5,13 @@ library(shinydashboard)
 source("/media/nick/Data/Users/N/Documents/MSc_Bioinfo/2016/Data_Analysis_Project/Circadian_Rhythm/DAM1_reader.R")
 source("/media/nick/Data/Users/N/Documents/MSc_Bioinfo/2016/Data_Analysis_Project/Circadian_Rhythm/actoplot_v4.R")
 
-
+PATH1 = "/media/nick/Data/Users/N/Documents/MSc_Bioinfo/2016/Data_Analysis_Project/Circadian_Rhythm/per_rescue_v2/120115A5M"
+dammulti1 = DAM1_multi_reader(PATH1, time_format = "min")
 
 ui <- navbarPage(theme = shinytheme("flatly"),
     title = "Analysis of Circadian Rhythm",
     id = "inTabset",
+    footer = column(6, offset = 3, br(), hr(), "Nicholas Li, Imperial College London, 2017"),
     HTML("<a name='top'></a>"),
     tabPanel("Home",
              fluidRow(
@@ -45,7 +47,7 @@ ui <- navbarPage(theme = shinytheme("flatly"),
                       h5("Below is a GUI for the function", code("actoplot()"), "with limited optional arguements available purely for demonstration. The full function is more flexible.", br(), "Please be patient as it may take time to load data."))
              ),
              selectInput("dataset", label = h3("Select example data to load"), 
-                         choices = c("dam1ex1", "dam1ex2"),
+                         choices = c("dam1ex1", "dam1ex2", "dammulti1"),
                          width = "20%"), 
              actionButton('loaddata', 'Load data'),
              br(), br(), br(),
@@ -62,21 +64,24 @@ ui <- navbarPage(theme = shinytheme("flatly"),
              #   )),
              fluidRow(column(2, offset = 7, HTML("<a href='#top'>top of page</a>")))
              ),
-    tabPanel("Methods and Results",
+    navbarMenu(title = "Other Methods",
+    tabPanel("Autocorrelation",
              withMathJax(),
              fluidRow(
                column(6, offset = 3, 
+                      h3("Autocorrelation"), "An actogram is useful for qualitative analysis, but often more quantitative methods are needed for decisive conclusions. One way is autocorrelation. ",
+                      helpText("$$r=\\frac{\\sum_{i=0}^n i^2}{}$$"),
                       helpText('An irrational number \\(\\sqrt{2}\\)
            and a fraction $$1-\\frac{1}{2}$$'))
              )
-             )
+             ))
 )
 
 
 server <- function(input, output, session) {
   observeEvent(input$nextpage2, {
     updateTabsetPanel(session, "inTabset",
-                      selected = "Methods and Results")
+                      selected = "Autocorrelation")
   })
   observeEvent(input$prevpage1, {
     updateTabsetPanel(session, "inTabset",
@@ -91,6 +96,7 @@ server <- function(input, output, session) {
     withProgress(message = "loading", value = 0,{
     dam1ex1 = DAM1_single_reader("/media/nick/Data/Users/N/Documents/MSc_Bioinfo/2016/Data_Analysis_Project/Circadian_Rhythm/per_rescue_v2/120115A5M/120115A5mCtM007C03.txt")
     dam1ex2 = DAM1_single_reader("/media/nick/Data/Users/N/Documents/MSc_Bioinfo/2016/Data_Analysis_Project/Circadian_Rhythm/Estaban_new_data/Circadian_data_for_Nicholas/220914es5/220914es5CtM011C27.txt")
+    
     output$sidePanel <- renderUI(sidebarPanel(
       sliderInput("dup_num", label = h3("Number of duplicated days:"),
                   min = 1, max = 10, value = 2),
@@ -120,7 +126,7 @@ server <- function(input, output, session) {
       conditionalPanel("input.plot_type == 'line'||input.plot_type == 'ribbon'||input.plot_type == 'tile'", "DD and LD options only available for bar plots currently"),
       hr(),
       actionButton(inputId = "go",
-                   label = "Update Plot")
+                   label = "Plot graph")
     ))
     
     data <- eventReactive(input$go, {actoplot_dam1(isolate(datasetInput()),
@@ -143,12 +149,14 @@ server <- function(input, output, session) {
     datasetInput <- reactive({
       switch(input$dataset,
            "dam1ex1" = dam1ex1,
-           "dam1ex2" = dam1ex2)
+           "dam1ex2" = dam1ex2,
+           "dammulti1" = dammulti1)
       })
     datasetText <- reactive({
       switch(input$dataset,
-             "dam1ex1" = "Displayed below is raw example data from DAM1 machines.",
-             "dam1ex2" = "Displayed below is raw example data from DAM2 machines.")
+             "dam1ex1" = "Displayed below is an example  raw data from DAM1 machines.",
+             "dam1ex2" = "Displayed below is raw example data from DAM1 machines.",
+             "dammulti1" = "Displayed below is population ")
       })
     output$hline <- renderUI(hr())
     output$ex1text <- renderText(
